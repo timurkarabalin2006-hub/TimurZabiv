aa28925ce7a3b9bf5e6ae6e7cd6c7a6fcaadeb93
timurkarabalin2006-hub <timur.karabalin2006@gmail.com>
2025-09-09 22:09:04 +0500

    Create pythonoption.py

---
+++ pythonoption.py
@@ -0,0 +1,8 @@
+
+name = input("Your name? ")
+subject = input("What is your favorite subject at school? ")
+class_number = input("Which class are you in? ")
+
+print("Your name" , name)
+print("Your favorite subject at school" , subject)
+print("You're in class number" , class_number)
