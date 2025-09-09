1b660066485ddd77ee851c928a5fe23c82ea5e09
timurkarabalin2006-hub <timur.karabalin2006@gmail.com>
2025-09-09 17:30:45 +0500

    Update pythonlaboratorywork.py

--- pythonlaboratorywork.py
+++ pythonlaboratorywork.py
@@ -1,7 +1,14 @@
+# Task 3
+# Запрашиваем ввод
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
-name = input("Your name? ")
-subject = input("What is your favorite subject at school? ")
-class_number = input("Which class are you in? ")
-print(name)
-print(subject)
-print(class_number)
+# Операции
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")   # обычное деление
+print(f"{a} // {b} = {a // b}") # целочисленное деление
+print(f"{a} % {b} = {a % b}")   # остаток от деления
+print(f"{a}^2 = {a ** 2}")      # возведение в квадрат
+print(f"{b}^2 = {b ** 2}")
