ae65198ef40a2684f14a459a542d4b0dd261ab53
timurkarabalin2006-hub <timur.karabalin2006@gmail.com>
2025-09-09 17:26:35 +0500

    Create pythonlaboratorywork.py

---
+++ pythonlaboratorywork.py
@@ -0,0 +1,7 @@
+
+name = input("Your name? ")
+subject = input("What is your favorite subject at school? ")
+class_number = input("Which class are you in? ")
+print(name)
+print(subject)
+print(class_number)
