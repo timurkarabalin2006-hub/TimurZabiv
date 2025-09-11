1e5ae8bc379611dcaef8ed8c938f26a004cf6bd8
timurkarabalin2006-hub <timur.karabalin2006@gmail.com>
2025-09-11 17:52:04 +0500

    typescript

--- typescript/math.js
+++
@@ -1,8 +0,0 @@
-var a = 14;
-var b = "hello";
-var c = true;
-var d = undefined;
-var e = null;
-var f = Symbol("555");
-var g = 124n;
-console.log(a);
--- typescript/math.ts
+++
@@ -1,8 +0,0 @@
-const a:number = 14;
-const b:string = "hello";
-const c:boolean = true;
-const d:undefined = undefined;
-const e:null = null
-const f:symbol = Symbol("555")
-const g:bigint = 124n // 
-console.log(a)
