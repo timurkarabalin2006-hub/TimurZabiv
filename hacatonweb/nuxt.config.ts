b59f84026abdc7515293589704781d530e7e499c
timurkarabalin2006-hub <timur.karabalin2006@gmail.com>
2025-12-04 20:46:38 +0500

    Update nuxt.config.ts

--- hacatonweb/nuxt.config.ts
+++ hacatonweb/nuxt.config.ts
@@ -1,10 +1,11 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
   modules: [
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/i18n'
   ],
 
   css: [
